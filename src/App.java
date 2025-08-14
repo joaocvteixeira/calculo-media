6afe81c606af8067706b5efbb38ad36d840d186e
João Carlos Viana Teixeira <joaocvteixeira@outlook.com>
2025-08-14 16:30:30 -0300

    adicionando uso do scanner

--- src/App.java
+++ src/App.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     
-    public static int calculoDeMedia(int numero1, int numero2) {
-        int media = (numero1 + numero2) / 2;
+    public static double calculoDeMedia(double numero1, double numero2) {
+        double media = (numero1 + numero2) / 2;
         return media;
     }
 
     public static void main(String[] args) throws Exception {
-        int notaDoJoao = calculoDeMedia(5, 9);
-        System.out.println("A média das notas de João é de " + notaDoJoao + " Pontos");
+        int nota1;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite a nota da primeira avaliação do aluno: ");
+        nota1 = ler.nextInt();
+
+        int nota2;
+        System.out.println("Digite a nota da segunda avaliação do aluno: ");
+        nota2 = ler.nextInt();
+
+        double notaDoAluno = calculoDeMedia(nota1, nota2);
+        System.out.println("A média das notas de João é de " + notaDoAluno + " Pontos");
     }
 
 }
